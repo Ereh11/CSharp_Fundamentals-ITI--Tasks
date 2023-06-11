04d0eb5e702e539bf87f38fc836227c3f481cf68
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-06-12 00:13:10 +0300

    Task 2 Done!

---
+++ Task 2/HiringDate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal struct HiringDate
+    {
+        private int _Day, _Month, _Year;
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public int Day
+        {
+            set
+            {
+                if (value > 0 && value <= 31)
+                    _Day = value;
+                else
+                    _Day = 1;
+            }
+            get => _Day;
+        }
+
+        public int Month
+        {
+            set
+            {
+                if(value > 0 && value <= 12)
+                    _Month = value;
+                else
+                    _Month = 1;
+            }
+            get => _Month;
+        }
+
+        public int Year
+        {
+            set
+            {
+                if (value > 2000 && value <= 2023)
+                    _Year = value;
+                else
+                    _Year = 2023;
+            }
+            get => _Year;
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] emp = new Employee[3];
+
+            // Read Data From End-User.
+            for(int i = 0; i < emp?.Length; i++)
+            {
+                Console.WriteLine($"Data Of Use No. {i + 1}");
+                int id, day, month, year;
+                Gender gndr;
+                decimal salary;
+                SecurityLevl sl;
+                do
+                {
+                    Console.WriteLine($"Enter User ID: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out id));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Salary: ");
+                }
+                while (!decimal.TryParse(Console.ReadLine(), out salary));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Gender No.{i + 1}: ");
+                }
+                while (!Gender.TryParse(Console.ReadLine(), out gndr));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Hiring Date day: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out day));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Hiring Date month: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out month));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Hiring Date year: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out year));
+
+                do
+                {
+                    Console.WriteLine($"Enter User Security Level: ");
+                }
+                while (!SecurityLevl.TryParse(Console.ReadLine(), out sl));
+
+                emp[i] = new Employee(id, gndr, new HiringDate(day, month, year), salary, sl);
+            }
+
+
+
+            // Sort Array Base on Hiring Date.
+            if (emp is not null)
+                Array.Sort(emp);
+
+            for(int i = 0; i < emp?.Length; i++)
+            {
+                // Print Data Of End-Users.
+                Console.WriteLine($"The following Data of Employeee No. {i + 1}");
+                Console.WriteLine(emp[i].ToString());
+            }
+        }
+    }
+}
